d8dc880ed1456998f130c7a0b7b46b2a85e93de7
Valentin Garnier <valentin.garnier@epitech.eu>
2022-12-08 19:09:42 +0100

    Fix deviles

--- my-app/vue.config.js
+++ my-app/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    disableHostCheck: true,
     devServer: {
+      disableHostCheck: true,
       proxy: {
         '^/api': {
           target: process.env.API_HOST + ':3070',
